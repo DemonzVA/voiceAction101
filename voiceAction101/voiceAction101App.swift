ba985f4793c860d86312c870db8f9dcd94697012
Matthew <matthewhanna04@gmail.com>
2024-03-04 13:40:16 -0500

    Initial Commit

---
+++ voiceAction101/voiceAction101App.swift
@@ -0,0 +1,17 @@
+//
+//  voiceAction101App.swift
+//  voiceAction101
+//
+//  Created by Matthew Hanna on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceAction101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
